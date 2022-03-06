641523e19c3da6ebd48568cd6b1a5ab03d539d8c
ansel brandt <ansel@anselbrandt.com>
2022-03-06 15:00:30 -0500

    Update

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,20 +1,98 @@
-import { Box, ThemeProvider } from "@primer/react";
+import {
+  FileDirectoryIcon,
+  GitBranchIcon,
+  PersonIcon,
+  SearchIcon,
+  ToolsIcon,
+} from "@primer/octicons-react";
+import { Box, ThemeProvider, useTheme } from "@primer/react";
 import type { NextPage } from "next";
+import { useState } from "react";
+import ColorModeSwitcher from "../components/ColorModeSwitcher";
+import IconPair from "../components/IconPair";
+import SideNav from "../components/SideNav";
 
-const Page: NextPage = () => {
+declare type ColorMode = "day" | "night";
+declare type ColorModeWithAuto = ColorMode | "auto";
+
+interface Props {
+  newFileCount?: number;
+  pendingChangesCount?: number;
+}
+
+const Page: NextPage<Props> = ({ newFileCount, pendingChangesCount }) => {
+  const size = 24;
+
+  const { resolvedColorMode } = useTheme();
+  const [colorMode, setColorMode] = useState<ColorModeWithAuto>(
+    resolvedColorMode || "day"
+  );
+  const [selectedTab, setSelectedTab] = useState("explorer");
+
+  const handleSelectedTab = (tab: string) => {
+    setSelectedTab(tab);
+    console.log(tab);
+  };
+
   return (
-    <ThemeProvider>
-      <Box
-        height="100vh"
-        width="100vw"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        bg="canvas.default"
-        color="fg.default"
-      >
-        <Box>Page</Box>
+    <ThemeProvider colorMode={colorMode}>
+      <Box height="100vh" bg="canvas.default" color="fg.default">
+        <ColorModeSwitcher setColorMode={setColorMode} />
+        <Box
+          sx={{
+            ">*": {
+              margin: 2,
+            },
+          }}
+        ></Box>
+        <Box>
+          <SideNav>
+            <SideNav.Link
+              onClick={() => handleSelectedTab("explorer")}
+              selected={selectedTab === "explorer"}
+            >
+              <IconPair
+                size={size}
+                icon={FileDirectoryIcon}
+                count={
+                  newFileCount && newFileCount > 0 ? newFileCount : undefined
+                }
+              />
+            </SideNav.Link>
+            <SideNav.Link
+              onClick={() => handleSelectedTab("search")}
+              selected={selectedTab === "search"}
+            >
+              <IconPair size={size} icon={SearchIcon} />
+            </SideNav.Link>
+            <SideNav.Link
+              onClick={() => handleSelectedTab("git")}
+              selected={selectedTab === "git"}
+            >
+              <IconPair
+                size={size}
+                icon={GitBranchIcon}
+                count={
+                  pendingChangesCount && pendingChangesCount > 0
+                    ? pendingChangesCount
+                    : undefined
+                }
+              />
+            </SideNav.Link>
+            <SideNav.Link
+              onClick={() => handleSelectedTab("settings")}
+              selected={selectedTab === "settings"}
+            >
+              <IconPair size={size} icon={ToolsIcon} />
+            </SideNav.Link>
+            <SideNav.Link
+              onClick={() => handleSelectedTab("user")}
+              selected={selectedTab === "user"}
+            >
+              <IconPair size={size} icon={PersonIcon} />
+            </SideNav.Link>
+          </SideNav>
+        </Box>
       </Box>
     </ThemeProvider>
   );
